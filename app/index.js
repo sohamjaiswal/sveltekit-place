d17ff92062cbf3410116fce208fd286e6b991522
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-10-10 13:19:30 +0530

    feat: prod env setup

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import { handler } from './build/handler.js';
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
 
@@ -11,6 +12,6 @@ app.get('/healthcheck', (req, res) => {
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
 
-app.listen(3000, () => {
+app.listen(process.env.APP_PORT, () => {
     console.log('🚀 listening on port 3000');
 });
