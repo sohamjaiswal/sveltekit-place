5eaf8483f59a1292a544e6c98fdbbbea31d4238b
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-10-10 13:35:10 +0530

    fix: use svelte adapter node

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,22 +1,36 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
+
 	vitePlugin: {
-		inspector: true,
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			precompress: true,
+		}),
+		csrf: {
+			checkOrigin: false,
+		},
+		version: {
+            name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			pollInterval: 10000
+		},
 	}
 };
 export default config;
