16999819d7cd8dba98879d03f6b47b400feb777c
Soham Jaiswal <sohamjaiswal10@gmail.com>
2024-04-18 19:45:24 +0530

    fix: leaderboard box sizing

--- app/src/routes/(public)/leaderboard/+page.svelte
+++ app/src/routes/(public)/leaderboard/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { guildedMediaLink } from '$lib/common/guilded-media.js';
 	import { getColorsFromImage } from '$lib/common/utils.js';
 	import { Avatar } from '@skeletonlabs/skeleton';
   export let data
 
   function getTextColor(element: HTMLDivElement, imageURL: string) {
     getColorsFromImage(imageURL, 10, 4).then((color) => {
+      console.log(imageURL)
       if (!color) {
         element.style.color = "#000"
         element.style.textShadow = `0 0 5px #fff`
@@ -26,20 +28,20 @@
   <h1>
     LEADERBOARD
   </h1>
-  <div class="h-[calc(100vh-3rem)] overflow-y-auto overflow-x-visible hide-scrollbar">
+  <div class="h-[calc(100vh-3rem)] overflow-y-auto overflow-x-visible hide-scrollbar w-full">
     {#each topChangers as topChanger}
-    <a class={`card p-4 w-full bg-no-repeat bg-cover card-hover`} style={`background-image: url('${topChanger.banner}');`} target="_blank" href={`https://guilded.gg/profile/${topChanger.id}`}>
-      <div class="card-body">
-        <div class="flex flex-row justify-between">
-          <div class="flex gap-4 items-center" use:getTextColor={topChanger.banner}>
+    <a class="w-full" target="_blank" href={`https://guilded.gg/profile/${topChanger.id}`}>
+      <div style={`background-image: url('${guildedMediaLink(topChanger.banner)}')`} class="card bg-no-repeat bg-cover w-full flex justify-between p-4" >
+        <div class="flex flex-row justify-between w-full">
+          <div class="flex gap-4 items-center" use:getTextColor={guildedMediaLink(topChanger.banner)}>
             <Avatar src={topChanger.avatar} width="w-16" rounded="rounded-none" />
             <h2 class="text-center">{topChanger.username}</h2>
           </div>
-          <div class="flex flex-col items-center" use:getTextColor={topChanger.banner}>
+          <div class="flex flex-col items-center" use:getTextColor={guildedMediaLink(topChanger.banner)}>
             <h3 class="text-center">Points</h3>
             <h3 class="text-center">{topChanger.totalPixelsChanged}</h3>
           </div>
-        </div>
+      </div>
       </div>
     </a>
     {/each}
